2d45d66f1a2621b5d2166e8fd571ce372526ae7b
Juan Francisco <jfsanchez@ext.chequemotiva.com.>
2025-02-28 16:36:09 +0100

    Just some cleaning

--- src/modules/admin/admin.module.ts
+++ src/modules/admin/admin.module.ts
@@ -3,7 +3,6 @@ import { AdminController } from './admin.controller';
 import { AdminService } from './admin.service';
 
 @Module({
-  imports: [],
   controllers: [AdminController],
   providers: [AdminService],
 })
